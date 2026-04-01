139d9049a6c50daa340eda904ac737718c3cf3f4
cekwedike <c.ekwedike@alustudent.com>
2026-04-01 19:26:58 +0200

    blog fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import vercel from '@astrojs/vercel'
 
 export default defineConfig({
   site: 'https://www.siryushub.com',
-  output: 'static',
+  output: 'hybrid',
   adapter: vercel(),
   integrations: [
     sitemap({
