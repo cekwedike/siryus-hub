724ed8282c746b50c7f60008d41a4743a60019b5
cekwedike <c.ekwedike@alustudent.com>
2026-04-04 03:17:30 +0200

    fix: disable CDN usage in Sanity client configuration

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -5,7 +5,7 @@ import type { SanityImageSource } from '@sanity/image-url'
 export const sanityClient = createClient({
   projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
   dataset: import.meta.env.PUBLIC_SANITY_DATASET,
-  useCdn: true,
+  useCdn: false,
   apiVersion: '2024-01-01',
 })
 
